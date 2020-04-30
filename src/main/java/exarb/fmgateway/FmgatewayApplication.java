46660ad494df67f2377b5d72603d3b3738a6f41a
Lovisa <lovisa.lybeck@yh.nackademin.se>
2020-04-30 16:04:42 +0200

    initial commit

---
+++ src/main/java/exarb/fmgateway/FmgatewayApplication.java
@@ -0,0 +1,13 @@
+package exarb.fmgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmgatewayApplication.class, args);
+	}
+
+}
